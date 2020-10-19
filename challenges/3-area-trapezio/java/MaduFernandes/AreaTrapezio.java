9d58f2b1dacf7fe2092a7a1dff51cc8afa240a34
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-10-19 11:18:05 -0300

    Finaliza Desafio

---
+++ challenges/3-area-trapezio/java/MaduFernandes/AreaTrapezio.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class AreaTrapezio {
+
+    public static void main(String[] args) {
+        areaTrapezio();
+    }
+
+    public static Integer areaTrapezio() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Informe a base maior do triangulo: ");
+        Integer baseMaior = input.nextInt();
+
+        if (baseMaior != 0) {
+            System.out.println("Informe a base menor do triangulo: ");
+            Integer baseMenor = input.nextInt();
+
+            if (baseMenor != 0) {
+                System.out.println("Informe a altura do triangulo: ");
+                Integer altura = input.nextInt();
+
+                if (altura != 0) {
+                    Integer areaDoTapezio = calcula(baseMaior, baseMenor, altura);
+                    System.out.println("A área do trapézio é: " + areaDoTapezio);
+                }
+                System.exit(0);
+            }
+        }
+        System.out.println("Por favor, informe um valor válido");
+        return areaTrapezio();
+    }
+
+    public static Integer calcula(Integer baseMaior, Integer baseMenor, Integer altura) {
+        Integer area = (baseMaior + baseMenor) * altura / 2;
+
+        return area;
+    }
+}
---
+++ challenges/3-area-trapezio/java/MaduFernandes/Readme.md
@@ -0,0 +1,24 @@
+# Submissão de Exercicio
+
+**Exercicio:** 3 - Área de um trapézio
+
+**Nickname:** MaduFernandes
+
+**Nível Técnico:** Estágio
+
+**Dificuldade de Resolução:** Baixa
+
+**Como rodar o desafio**:
+
+```bash
+
+Para executar esse projeto rode esse comando no terminal para compilar esse arquivo:
+
+cd /challenges/3-area-trapezio/java/MaduFernandes
+
+javac AreaTrapezio.java
+
+E depois rode essse comando para executar:
+
+java AreaTrapezio
+```
